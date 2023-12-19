280209fcff18230f50b075285973521007f675ab
Justin Cucuzza <jcucuzza2000@gmail.com>
2023-12-19 11:31:13 -0600

    link rename

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Welcome!
 
-This is a site for me to play around with React (specifically Next.js) and create a portfolio out of it. React may be overkill for this, but I enjoy it enough that I wanted to use it for this. I also like Next.js's server-side rendering.
+This is a site for me to play around with React (specifically Next.js) and create a portfolio out of it. React may be overkill for this, but I enjoy it enough that I wanted to use it for this. I also like Next.js's server-side rendering. You can view the portfolio by going to [this website](https://www.dippingsauce.net)
 
 This made with [Next.js](https://nextjs.org/) bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
             <div className="w-full md:flex md:items-center md:w-auto"><a href="mailto:jcucuzza@pm.me">Contact Me</a></div>
             <div className=" md:flex md:items-center md:w-auto">
                 <a className="md:p-4 py-2 flex" href="https://www.linkedin.com/in/justin-cucuzza/"><Image src="/static/linkedin.svg" alt="linkedin logo" width="32" height="32"/></a>
-                <a className="md:p-4 py-2 flex" href="https://github.com/d1ppingsauce"><Image src="/static/github.svg" alt="github logo" width="32" height="32"/></a>
+                <a className="md:p-4 py-2 flex" href="https://github.com/j-cucuzza"><Image src="/static/github.svg" alt="github logo" width="32" height="32"/></a>
             </div>
         </footer>
     )
